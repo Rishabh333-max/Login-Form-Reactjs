a37b8c0920c62cbb21b7fbb56fde54adbd771736
Rishabh333-max <81370987+Rishabh333-max@users.noreply.github.com>
2021-08-17 16:09:04 +0530

    LOgin form created by using React js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App=()=>{
+  const[fullName,setFullName]=useState({
+    fname:"",
+    lname:"",
+    email:"",
+    number:""
+  });
+
+  const inputEvent=(e)=>{
+      console.log(e.target.value);
+      console.log(e.target.name);
+
+      const name=e.target.name;
+      const value=e.target.value;
+      const email=e.target.email;
+      const number=e.target.number;
+
+     setFullName((preValue)=>{
+       if(name==="fname"){
+        return {fname:value,
+        lname:preValue.lname,
+        email:preValue.email,
+        number:preValue.number}
+       }
+       else if(name==="lname"){
+
+       return {
+        lname:value, 
+        fname:preValue.fname,
+        number:preValue.number,
+ 
+      email:preValue.email}
+       }
+       else if(name==="email"){
+        return {  email:value,
+           fname:preValue.fname,
+         lname:preValue.lname,
+         number:preValue.number
+      }
+        }
+        else if(name==="number"){
+          return {   number:value,
+             email:preValue.email,
+             fname:preValue.fname,
+           lname:preValue.lname,
+         
+        }
+          }
+     
+    })
+     
+  }
+  const onSubmit=(e)=>{
+    e.preventDefault();
+    alert("Form Submitted")
+
+  }
+  
+  return(
+    <>
+    <div className="main_div">
+      <form onSubmit={onSubmit}>
+    <div>
+    <h1>Hello {fullName.fname} {fullName.lname}  </h1>
+    <h3>{fullName.email}</h3>
+    <h4>{fullName.number}</h4>
+    <input type="text" placeholder="Enter Your Name"  name="fname" onChange={inputEvent}  autoSave="off"  autoComplete="off"
+    //  value={fullName.fname}
+     >  
+    </input>
+    <input type="text" placeholder="Enter Your Last Name" name="lname" onChange={inputEvent}  autoSave="off" autoComplete="off"
+    //  value={fullName.lname} 
+    ></input>
+     <input type="email" placeholder="Enter Your E-mail" name="email" onChange={inputEvent} autoSave="off" autoComplete="off"></input>
+     <input type="number" placeholder="Enter Your Mobile Number" name="number" onChange={inputEvent}  autoSave="off" autoComplete="off" ></input>
+   
+    <button type="submit">Submit 👍</button>
     </div>
-  );
-}
+    </form>
+    </div>
+    </>
 
+  )
+}
 export default App;
